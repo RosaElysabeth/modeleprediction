a87d151709f21092e14e9d8bd482c44c307a6b6e
Mona Lisa <ralinirinarosa7@gmail.com>
2024-01-22 10:04:45 +0300

    add jupyter notebook file

--- model.py
+++ model.py
@@ -5,73 +5,72 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.impute import SimpleImputer
 import shap
-import time 
+import time
 
-def charger_modele():
-    start_time = time.time()  # Enregistrement du temps de début
+def load_model():
+    start_time = time.time()  # Recording start time
     
-    # Chargement des données
-    print("Chargement des données...")
+    # Loading data
+    print("Loading data...")
     data = pd.read_excel('./etat_securite.xlsx')
-    print("Données chargées avec succès.")
+    print("Data loaded successfully.")
     
     df = data.copy()
     
-    # Prétraitement des données
+    # Data preprocessing
     features = df.drop('Situation_Surpoids', axis=1)
     target = df['Situation_Surpoids']
     
-    # Encodage
+    # Encoding
     code = {'Acceptable(Normale)': 1, 'Précaire': 2, 'Alarmante(Alerte)': 3, 'Critique(Urgence)': 4}  
     for col in df.select_dtypes('object').columns:
         df.loc[:, col] = df[col].replace(code)
     
-    # Imputation des valeurs manquantes pour toutes les colonnes avec la stratégie 'most_frequent'
+    # Imputing missing values for all columns with 'most_frequent' strategy
     imputer = SimpleImputer(strategy='most_frequent')
     df_imputed = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
     
-    # Division des données en ensembles d'entraînement et de test
+    # Splitting data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(df_imputed, target, test_size=0.2, random_state=42)
     
-    print(f"Debug - Forme des données : {df.shape}")
-    print(f"Debug - Forme de X_train : {X_train.shape}, Forme de y_train : {y_train.shape}")
+    print(f"Debug - Data Shape: {df.shape}")
+    print(f"Debug - X_train Shape: {X_train.shape}, y_train Shape: {y_train.shape}")
 
-    # Création et entraînement du modèle
-    print("Entraînement du modèle...")
-    modele_securite_alimentaire = SVC(kernel='linear', C=1)
-    modele_securite_alimentaire.fit(X_train, y_train)
-    print("Modèle entraîné avec succès.")
+    # Creating and training the model
+    print("Training the model...")
+    food_security_model = SVC(kernel='linear', C=1)
+    food_security_model.fit(X_train, y_train)
+    print("Model trained successfully.")
 
-    # Initialisation de SHAP avec le modèle entraîné
-    print("Initialisation de SHAP...")
-    explainer = shap.Explainer(modele_securite_alimentaire, X_train)
-    print("SHAP initialisé avec succès.")
+    # Initializing SHAP with the trained model
+    print("Initializing SHAP...")
+    explainer = shap.Explainer(food_security_model, X_train)
+    print("SHAP initialized successfully.")
 
-    
-    # Enregistrement du temps de fin et calcul de la durée
+    # Recording end time and calculating duration
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"Temps de chargement du modèle : {elapsed_time:.2f} secondes")
+    print(f"Model loading time: {elapsed_time:.2f} seconds")
     
-    return modele_securite_alimentaire, explainer, X_train, y_train
+    return food_security_model, explainer, X_train, y_train
 
-def predire(modele, features):
-    modele_securite_alimentaire = modele
-    prediction = modele_securite_alimentaire.predict(features)[0]
+def predict(model, features):
+    food_security_model = model
+    prediction = food_security_model.predict(features)[0]
     return prediction
 
-def calculer_shap(modele, features):
-    _, explainer = modele
+def calculate_shap(model, features):
+    _, explainer = model
     features = features.apply(pd.to_numeric, errors='coerce')
     
-    # Calcul des valeurs SHAP
+    # Calculating SHAP values
     shap_values = explainer.shap_values(features)
     
     # print(shap_values)
 
     return shap_values
 
-# Fonction pour mapper les noms de région à des valeurs numériques
+# Function to map region names to numeric values
 def map_region_to_numeric(region_name):
     region_mapping = {
         "madagascar": 0,
@@ -100,10 +99,10 @@ def map_region_to_numeric(region_name):
         "androy": 23
     }
 
-    # Convertir la région en minuscules avant de chercher dans le dictionnaire
+    # Convert region to lowercase before looking in the dictionary
     region_name_lower = region_name.lower()
 
-    # Retourne la valeur numérique correspondante si elle existe, sinon retourne -1 ou une valeur par défaut
+    # Return the corresponding numeric value if it exists, otherwise return -1 or a default value
     return region_mapping.get(region_name_lower, -1)
 
 def interpret_shap(feature_names, shap_values, region_name):
@@ -113,43 +112,42 @@ def interpret_shap(feature_names, shap_values, region_name):
         for i, val in enumerate(shap_values):
             feature_name = feature_names[i]
             interpretations.append(
-                f"Pour la région {region_name}, la valeur de la fonctionnalité '{feature_name}' a un impact de {val:.4f} sur la prédiction."
+                f"For the region {region_name}, the value of the feature '{feature_name}' has an impact of {val:.4f} on the prediction."
             )
     else:
         interpretations.append(
-            f"Pour la région {region_name}, la valeur de la fonctionnalité a plusieurs composantes et ne peut pas être interprétée de manière simple. Les composantes sont : {shap_value.tolist()}"
+            f"For the region {region_name}, the value of the feature has multiple components and cannot be interpreted simply. The components are: {shap_value.tolist()}"
         )
 
     return interpretations
 
-
 def main():
-    st.title("Prédiction des phénomènes")
+    st.title("Prediction of Phenomena")
 
-    # Charger le modèle
-    modele, explainer, X_train, y_train = charger_modele()
+    # Load the model
+    model, explainer, X_train, y_train = load_model()
 
-    # Interface utilisateur pour saisir les fonctionnalités
-    region_name = st.text_input("Entrez la région:")
+    # User interface to input features
+    region_name = st.text_input("Enter the region:")
     region_numeric = map_region_to_numeric(region_name)
 
-    # Vérifier si la région est valide
+    # Check if the region is valid
     if region_numeric == -1:
-        st.warning("La région saisie n'est pas valide. Veuillez saisir une région valide.")
+        st.warning("The entered region is not valid. Please enter a valid region.")
         return
 
-    date = st.date_input("Entrez la date:")
+    date = st.date_input("Enter the date:")
 
     # Extract features from date
     year = date.year
     month = date.month
     day = date.day
 
-    # Bouton pour effectuer la prédiction
-    if st.button("Prédire"):
-        start_prediction_time = time.time()  # Enregistrez le temps de début de prédiction
+    # Button to make the prediction
+    if st.button("Predict"):
+        start_prediction_time = time.time()  # Record prediction start time
 
-          # Prétraitement des fonctionnalités (excluant "Situation_Surpoids")
+          # Preprocess features (excluding "Overweight_Situation")
         features1 = pd.DataFrame({
             "DATE": [year],
             "REGION": [region_numeric],
@@ -193,39 +191,39 @@ def main():
             "Pourcentage_membres_menages_lieu_lavage_mains_eau_savon_detergent": [26]
         })
         
-        # Utiliser les noms de colonnes de X_train pour garantir la cohérence
+        # Use X_train column names to ensure consistency
         features1.columns = X_train.columns
 
-        # Faire la prédiction
-        prediction1 = predire(modele, features1)
-        prediction2 = predire(modele, features2)
-        prediction3 = predire(modele, features3)
+        # Make predictions
+        prediction1 = predict(model, features1)
+        prediction2 = predict(model, features2)
+        prediction3 = predict(model, features3)
 
-        # Afficher la prédiction
-        st.write(f"Prédiction de l'insuffisance pondérale : {prediction1}")
-        st.write(f"Prédiction de la malnutrition chronique : {prediction2}")
-        st.write(f"Prédiction de la malnutrition aiguë : {prediction3}")
+        # Display predictions
+        st.write(f"Prediction of Underweight: {prediction1}")
+        st.write(f"Prediction of Chronic Malnutrition: {prediction2}")
+        st.write(f"Prediction of Acute Malnutrition: {prediction3}")
 
-        # Calculer et afficher les valeurs SHAP
-        shap_values = calculer_shap((modele, explainer), features1)
+        # Calculate and display SHAP values
+        shap_values = calculate_shap((model, explainer), features1)
         
-         # Enregistrement du temps de fin de prédiction et calcul de la durée
+         # Record end time of prediction and calculate duration
         end_prediction_time = time.time()
         elapsed_prediction_time = end_prediction_time - start_prediction_time
-        print(f"Temps de prédiction : {elapsed_prediction_time:.4f} secondes")
+        print(f"Prediction time: {elapsed_prediction_time:.4f} seconds")
 
-        # Obtenir l'ordre décroissant des indices des fonctionnalités par impact
+        # Get descending order of feature indices by impact
         feature_order = list(reversed(np.argsort(shap_values[0])))
 
         print(f"Debug - Feature Order: {feature_order}")
         
-        #Valeurs SHAP
-        st.write("Valeurs SHAP :")
+        #SHAP Values
+        st.write("SHAP Values:")
         st.write(f"{shap_values}")
 
-        # Afficher les résultats SHAP avec des commentaires adaptés aux nutritionnistes
+        # Display SHAP results with comments suitable for nutritionists
 
-        st.write("Interprétation des Valeurs SHAP :")
+        st.write("Interpretation of SHAP Values:")
 
         feature_names = features1.columns 
 
